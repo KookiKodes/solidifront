5b4bf1411fd576adec682e98f2e1a41cc6f86f7a
Kooki Kodes <kookikodes@gmail.com>
2024-10-06 16:25:36 -0700

    attempt at creating storefront client

--- examples/basic/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@solidjs/start/env" />
-interface ImportMetaEnv {
-  SHOPIFY_PUBLIC_STORE_DOMAIN: string;
-  SHOPIFY_STOREFRONT_API_VERSION: string;
-  SHOPIFY_PUBLIC_STOREFRONT_ACCESS_TOKEN: string;
-  SHOPIFY_PRIVATE_STORFRONT_TOKEN: string;
-}
--- examples/basic/src/global.d.ts
+++ examples/basic/src/global.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="@solidjs/start/env" />
+interface ImportMetaEnv {
+  SHOPIFY_PUBLIC_STORE_DOMAIN: string;
+  SHOPIFY_STOREFRONT_API_VERSION: string;
+  SHOPIFY_PUBLIC_STOREFRONT_ACCESS_TOKEN: string;
+  SHOPIFY_PRIVATE_STORFRONT_TOKEN: string;
+}
