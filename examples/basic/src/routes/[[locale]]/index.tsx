e4de9578cb7b511fc115903443d9912ad1c12801
Kooki Kodes <kookikodes@gmail.com>
2024-10-06 20:21:31 -0700

    fixed storefront cart middleware

--- examples/basic/src/routes/[[locale]]/index.tsx
+++ examples/basic/src/routes/[[locale]]/index.tsx
@@ -1,10 +1,19 @@
 import { useLocale } from "@solidifront/start";
 import { Title } from "@solidjs/meta";
+import { createAsync } from "@solidjs/router";
 import { createEffect } from "solid-js";
+import { getRequestEvent } from "solid-js/web";
 import Counter from "~/components/Counter";
+import { shopQuery } from "~/graphql/storefront/queries";
 
 export default function Home() {
   const locale = useLocale();
+  const data = createAsync(async () => {
+    "use server";
+    const req = await getRequestEvent()?.locals.storefront.query(shopQuery);
+
+    return req?.data?.shop.name;
+  });
 
   createEffect(() => {
     console.log(locale());
@@ -13,7 +22,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1>{data()}</h1>
       <Counter />
       <p>
         Visit{" "}
