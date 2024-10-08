b37ed0c2a8911bc9e3a36c1804b3fb7772b87f6d
Kooki Kodes <kookikodes@gmail.com>
2024-10-08 16:52:25 -0700

    removed stream handling, will figure out how to handle in future

--- examples/basic/src/routes/[[locale]]/index.tsx
+++ examples/basic/src/routes/[[locale]]/index.tsx
@@ -1,39 +1,46 @@
 import {
-  getStorefrontClient,
   createStorefrontQuery,
   storefront,
   useLocale,
 } from "@solidifront/start";
 import { Title } from "@solidjs/meta";
 import { createAsync } from "@solidjs/router";
-import { createEffect, createSignal, Show } from "solid-js";
+import { Show } from "solid-js";
 import Counter from "~/components/Counter";
 import { shopQuery } from "~/graphql/storefront/queries";
 
+export const route = {
+  async preload() {
+    return await storefront.query(shopQuery);
+  },
+};
+
 export default function Home() {
   const locale = useLocale();
-  const query = createStorefrontQuery(shopQuery);
+  const query = createAsync(() => storefront.query(shopQuery));
 
   return (
-    <main>
-      <Title>Hello World</Title>
+    <Show when={query()}>
+      <main>
+        <Title>Hello World</Title>
 
-      <h1>Hello</h1>
+        <h1>Hello {query()?.data?.shop?.name}</h1>
 
-      <div>
-        <Counter />
-        <p>
-          Visit{" "}
-          <a href="https://start.solidjs.com" target="_blank">
-            start.solidjs.com
-          </a>{" "}
-          to learn how to build SolidStart apps.
-        </p>
-      </div>
-      <pre>
-        <p>Locale: </p>
-        <code>{JSON.stringify(locale(), null, 2)}</code>
-      </pre>
-    </main>
+        <div>
+          <Counter />
+          <p>
+            Visit{" "}
+            <a href="https://start.solidjs.com" target="_blank">
+              start.solidjs.com
+            </a>{" "}
+            to learn how to build SolidStart apps.
+          </p>
+        </div>
+        <pre>
+          <p>Locale: </p>
+          <code>{JSON.stringify(locale(), null, 2)}</code>
+        </pre>
+      </main>
+    </Show>
   );
 }
