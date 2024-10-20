f2ec1a9b24eae4ffaf90d80da6bcbedc5522c59e
kookikodes <kookikodes@gmail.com>
2024-10-19 20:23:47 -0700

    Moved locale provider into app.tsx

--- examples/basic/src/app.tsx
+++ examples/basic/src/app.tsx
@@ -1,5 +1,5 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import { A } from "@solidifront/start/localization";
+import { A, LocaleProvider } from "@solidifront/start/localization";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -13,7 +13,9 @@ export default function App() {
           <Title>SolidStart - Basic</Title>
           <A href="/">Index</A>
           <A href="/about">About</A>
-          <Suspense>{props.children}</Suspense>
+          <Suspense>
+            <LocaleProvider>{props.children}</LocaleProvider>
+          </Suspense>
         </MetaProvider>
       )}
     >
--- examples/basic/src/routes/[[locale]].tsx
+++
@@ -1,6 +0,0 @@
-import { RouteSectionProps } from "@solidjs/router";
-import { LocaleProvider } from "@solidifront/start/localization";
-
-export default function Locale(props: RouteSectionProps) {
-  return <LocaleProvider>{props.children}</LocaleProvider>;
-}
